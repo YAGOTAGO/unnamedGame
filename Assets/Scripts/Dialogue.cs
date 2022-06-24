59397f2399d67463805f7718fb4d6b6fac31a269
YAGOTAGO <tiagocaetanodavies@gmail.com>
2022-06-24 16:39:39 -0400

    Added more descriptions

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -18,7 +18,7 @@ public class Dialogue : MonoBehaviour
     public List<string> dialogues;
 
     //Writing speed
-    public float writingSpeed = .02f;
+    public float writingSpeed = 0f;
     
     //Index on dialogue
     private int index;
